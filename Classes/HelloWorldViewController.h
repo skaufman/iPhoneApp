42926de05215126ccb9ef67ddb7e73c6740ae924
Stan Kaufman <skaufman@epimetrics.com>
2010-12-03 11:17:02 -0800

    initial load of HelloWorld project

---
+++ Classes/HelloWorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Stan Kaufman on 12/2/10.
+//  Copyright 2010 The Epimetrics Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+
+}
+
+@end
+
